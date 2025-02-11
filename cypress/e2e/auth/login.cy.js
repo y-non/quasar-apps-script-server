729ae71411e4e6248fb17c7e68495140cc8461b2
trankhoadev <trankhoadev@gmail.com>
2025-02-11 12:32:51 +0700

    chore: add mock data testing and use variable instead of string value for account user logging

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -1,20 +1,34 @@
 describe("Login Page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
   it("should load the login page", () => {
-    cy.visit("/"); // Change port if needed
     cy.contains("Đăng nhập").should("be.visible");
   });
 
   it("should allow a user to type username and password", () => {
-    cy.visit("/");
     cy.get('input[aria-label="Tên đăng nhập"]').type("testuser");
     cy.get('input[aria-label="Mật khẩu"]').type("password123");
   });
 
   it("should display an error for invalid login", () => {
-    cy.visit("/");
-    cy.get('input[aria-label="Tên đăng nhập"]').type("wronguser");
-    cy.get('input[aria-label="Mật khẩu"]').type("wrongpassword");
-    cy.get('button[type="submit"]').click();
-    cy.contains("Invalid login credentials").should("be.visible");
+    cy.fixture("users").then((users) => {
+      cy.get('input[aria-label="Tên đăng nhập"]').type(users.invalidUser.email);
+      cy.get('input[aria-label="Mật khẩu"]').type(users.invalidUser.password);
+      cy.get('button[type="submit"]').click();
+      cy.contains("Invalid login credentials").should("be.visible");
+    });
+  });
+
+  it("should login successfully", () => {
+    cy.fixture("users").then((users) => {
+      cy.get('input[aria-label="Tên đăng nhập"]').type(users.validUser.email);
+      cy.get('input[aria-label="Mật khẩu"]').type(users.validUser.password);
+      cy.get('button[type="submit"]').click();
+      setTimeout(() => {
+        cy.url().should("include", "/data");
+      }, 2000);
+    });
   });
 });
